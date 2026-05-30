cbd956d133c615580c36e0c8b94dbf0cea89e765
Shubhangi Bhosale <bhosaleshubhangi333@gmail.com>
2026-05-30 19:49:47 +0530

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "React Developer",
+          "Frontend Developer",
+          "JavaScript Developer",
+          "UI Engineer",
         ],
         autoStart: true,
         loop: true,
