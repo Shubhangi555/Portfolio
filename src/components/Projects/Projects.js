fc1d11c09e99bbb8b29e2004a660b117b6ad2586
Shubhangi Bhosale <bhosaleshubhangi333@gmail.com>
2026-05-30 21:07:54 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,7 +8,7 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
-import bitsOfCode from "../../Assets/Projects/project1.png";
+import project1 from "../../Assets/Projects/project1.png";
 
 function Projects() {
   return (
