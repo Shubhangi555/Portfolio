f282cb8e5e7a2338ca809b0f6992ed91699c74cf
Shubhangi Bhosale <bhosaleshubhangi333@gmail.com>
2026-05-30 20:21:56 +0530

    Update Particle.js

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -8,41 +8,67 @@ function Particle() {
       params={{
         particles: {
           number: {
-            value: 160,
+            value: 80,
             density: {
               enable: true,
-              value_area: 1500,
+              value_area: 1000,
             },
           },
+          color: {
+            value: "#c770f0",
+          },
           line_linked: {
-            enable: false,
-            opacity: 0.03,
+            enable: true,
+            distance: 150,
+            color: "#c770f0",
+            opacity: 0.2,
+            width: 1,
           },
           move: {
-            direction: "right",
-            speed: 0.05,
+            enable: true,
+            speed: 0.5,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
           },
           size: {
-            value: 1,
+            value: 2,
+            random: true,
           },
           opacity: {
+            value: 0.5,
+            random: false,
             anim: {
               enable: true,
               speed: 1,
-              opacity_min: 0.05,
+              opacity_min: 0.1,
+              sync: false,
             },
           },
         },
         interactivity: {
+          detect_on: "canvas",
           events: {
+            onhover: {
+              enable: true,
+              mode: "grab",
+            },
             onclick: {
               enable: true,
               mode: "push",
             },
+            resize: true,
           },
           modes: {
+            grab: {
+              distance: 140,
+              line_linked: {
+                opacity: 0.5,
+              },
+            },
             push: {
-              particles_nb: 1,
+              particles_nb: 2,
             },
           },
         },
